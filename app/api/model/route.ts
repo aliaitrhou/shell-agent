d871fce86998304a389a917c81467d60257e3999
aliaitrahou <alirhou7@gmail.com>
2024-11-25 17:38:51 +0100

    feat(api): Enginner a powerfull prompt

--- app/api/model/route.ts
+++ app/api/model/route.ts
@@ -7,9 +7,17 @@ export async function POST(req: Request) {
 
   const together = new Together();
 
+  const prompt = process.env.MAGIC_PROMPT || "";
+
   const runner = together.chat.completions.stream({
     model: "Qwen/Qwen2.5-7B-Instruct-Turbo",
-    messages: [{ role: "user", content: message }],
+    messages: [
+      {
+        role: "system",
+        content: prompt,
+      },
+      { role: "user", content: message },
+    ],
     temperature: 0.7,
     max_tokens: 200,
   });
@@ -17,40 +25,4 @@ export async function POST(req: Request) {
   return new Response(runner.toReadableStream());
 }
 
-export const runtime = "nodejs";
-
-// // Handle POST requests
-//
-//
-// const together = new Together({ apiKey: process.env.TOGETHER_API_KEY });
-//
-// export async function POST(request: Request) {
-//   const data = await request.json();
-//   const response = await together.chat.completions.create({
-//     messages: [
-//       {
-//         role: "system",
-//         content: "you are a helpfull assistent",
-//       },
-//       {
-//         role: "user",
-//         content: data.message,
-//       },
-//     ],
-//
-//     model: "meta-llama/Meta-Llama-3.1-405B-Instruct-Turbo",
-//     temperature: 0.7,
-//     top_p: 0.7,
-//     top_k: 50,
-//     repetition_penalty: 1,
-//     stop: ["<|eot_id|>", "<|eom_id|>"],
-//     stream: true,
-//   });
-//
-//   console.log(response);
-//
-//   return NextResponse.json({
-//     answer: data.message,
-//     // answer: response?.choices[0]?.message?.content || "No response from the AI",
-//   });
-// }
+export const runtime = "edge";
