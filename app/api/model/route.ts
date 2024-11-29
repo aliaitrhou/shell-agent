f7a4f3fd254b08c4beacff5dff82fff34eabb4f8
aliaitrahou <alirhou7@gmail.com>
2024-11-29 11:29:51 +0100

    feat(api): integrate teachers course data with the model responses

--- app/api/model/route.ts
+++ app/api/model/route.ts
@@ -1,21 +1,40 @@
 import Together from "together-ai";
+import { createClient } from "@supabase/supabase-js";
 
 if (!process.env.TOGETHER_API_KEY) throw new Error("Missing Together env var");
+const sbApiKey = process.env.SUPABASE_API_KEY || "";
+const sbURL = process.env.SUPABASE_URL || "";
 
 export async function POST(req: Request) {
   const { message, chatHistory } = await req.json();
-
-  console.log("History is:", chatHistory);
   const together = new Together();
 
-  const prompt = process.env.MAGIC_PROMPT || "";
+  // Get the query embedding for the message using together ai embedding model
+  const response = await together.embeddings.create({
+    model: "togethercomputer/m2-bert-80M-8k-retrieval",
+    input: message,
+  });
 
+  const supabase = createClient(sbURL, sbApiKey);
+
+  const { data, error } = await supabase.rpc("match_documents", {
+    query_embedding: response.data[0].embedding,
+    match_count: 2,
+  });
+
+  if (error) {
+    console.log(error);
+  } // This will return the matched documents from supabase
+
+  const prompt = process.env.MAGIC_PROMPT || "";
   const runner = together.chat.completions.stream({
     model: "Qwen/Qwen2.5-7B-Instruct-Turbo",
     messages: [
       {
         role: "system",
-        content: prompt + `CHAT_HISTORY:${JSON.stringify(chatHistory)}`,
+        content:
+          prompt +
+          `CHAT_HISTORY:${JSON.stringify(chatHistory)}\nCONTEXT: ${data.map((doc: any) => doc.content)}`,
       },
       { role: "user", content: message },
     ],
@@ -25,5 +44,4 @@ export async function POST(req: Request) {
 
   return new Response(runner.toReadableStream());
 }
-
-export const runtime = "edge";
+export const runtime = "nodejs";
