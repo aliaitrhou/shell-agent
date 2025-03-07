cd0a2783028727180cf56e128bd95f0d80bdd692
aliaitrahou <alirhou7@gmail.com>
2025-03-07 23:52:12 +0000

    feat(markdown): copy code snippets

--- components/MarkdownRenderer.tsx
+++ components/MarkdownRenderer.tsx
@@ -1,12 +1,14 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { FC, memo } from "react";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
 import { vscDarkPlus } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import remarkMath from "remark-math";
+import { FaRegCopy } from "react-icons/fa6";
+import { FaCheck } from "react-icons/fa6";
 
 interface Props {
   language: string;
@@ -18,10 +20,29 @@ type MarkdownRendererProps = {
 };
 
 const CodeBlock: FC<Props> = memo(({ language, value }) => {
+  const [copied, setCopied] = useState(false);
+
+  const handleCopyCode = (text: string) => {
+    console.log("text to copy is : ", text);
+    console.log("copy!");
+
+    navigator.clipboard.writeText(text);
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 3000);
+  };
+
   return (
-    <div className="relative w-full sm:w-fit  codeblock bg-zinc-900/60 border-[.3px] border-zinc-600 rounded-md overflow-hidden my-3">
-      <div className="flex items-center justify-between w-full px-3 py-1 pr-4 rounded-t bg-zinc-700/80 text-zinc-100 border-b-[.3px] border-zinc-600">
-        <span className="text-xs lowercase font-spaceMono">{language}</span>
+    <div className="relative w-full sm:w-fit ml-2  codeblock bg-zinc-900/60 border-[.3px] border-zinc-600 rounded-md overflow-hidden my-3">
+      <div className="flex items-center justify-between w-full text-xs px-3 py-1 rounded-t bg-zinc-900/40 text-zinc-400 border-b-[.3px] border-zinc-600">
+        <span className="lowercase font-spaceMono ">{language}</span>
+        <button
+          className="focus:outline-none"
+          onClick={() => handleCopyCode(value)}
+        >
+          {copied ? <FaCheck /> : <FaRegCopy className="hover:text-zinc-200" />}
+        </button>
       </div>
       <SyntaxHighlighter
         language={language}
@@ -106,7 +127,7 @@ const MarkdownRenderer: FC<MarkdownRendererProps> = ({ children }) => {
             <code
               className={
                 className ||
-                "bg-zinc-700 border border-zinc-600 px-1 py-0 rounded-md text-green-300"
+                "text-xs  bg-zinc-700 border border-zinc-600 px-1 rounded-md text-zinc-300"
               }
               {...props}
             >
