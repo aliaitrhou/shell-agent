a310af423e56567b658fcbbc4cd7c39c34ecc5ea
aliaitrahou <alirhou7@gmail.com>
2025-03-08 06:35:01 +0000

    refactor(layout): remove footer to main page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
-import Header from "@/components/hearder";
 import { Analytics } from "@vercel/analytics/react";
-import Footer from "@/components/footer";
 
 const clerkKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
 
@@ -35,9 +33,7 @@ export default function RootLayout({
       <html lang="en">
         <body className="bg-zinc-950">
           <div className="gradient_background"></div>
-          <Header />
           {children}
-          <Footer />
           <Analytics />
         </body>
       </html>
