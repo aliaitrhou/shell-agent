79fd740a35a0658cdd6f48b6814284d63573b191
aliaitrahou <alirhou7@gmail.com>
2025-03-11 23:28:11 +0000

    style(instructions): adjust compoment styles

--- components/instructions.tsx
+++ components/instructions.tsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { GiOpenBook } from "react-icons/gi";
+import { FaInfoCircle } from "react-icons/fa";
 
 const Instructions = () => {
   return (
-    <div className="max-w-md sm:max-w-lg md:max-w-xl mx-auto px-4 pt-2 pb-3 space-y-1">
-      <div className="pl-2 text-lg sm:text-2xl md:text-3xl flex items-center gap-2 sm:gap-3 ">
-        <GiOpenBook />
+    <div className="max-w-lg sm:max-w-xl md:max-w-3xl mx-auto px-4 pt-2 pb-3 space-y-1">
+      <div className="pl-2 text-xl sm:text-3xl md:text-4xl flex items-center gap-2 sm:gap-3 ">
+        <FaInfoCircle className="size-8" />
         <span className="font-kanit">Instructions</span>
       </div>
       <ul
         role="list"
-        className="list-disc font-light text-sm sm:text-lg  font-kanit text-zinc-400 pl-2 sm:pl-3 md:pl-4"
+        className="list-disc font-light text-sm sm:text-lg  font-kanit text-zinc-300 pl-2 sm:pl-3 md:pl-4"
       >
         <li>
           You are represanted with two modes{" "}
@@ -18,7 +18,7 @@ const Instructions = () => {
           <span className="text-emerald-500 font-semibold">Command</span>.
         </li>
         <li>
-          Use Prompt to learn the command and use Command to try it yourself
+          Use Prompt to learn the command then use Command to try it yourself
         </li>
         <li>
           Your starting point is user directory &apos;
@@ -33,6 +33,10 @@ const Instructions = () => {
         <li>
           Typing <span className="text-emerald-500 font-semibold">exit</span>{" "}
           with Command mode would close the terminal
+        </li>
+        <li>
+          If you want to know where the answer came from click on the page
+          number at the left side.
         </li>
       </ul>
     </div>
