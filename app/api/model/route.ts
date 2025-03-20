6091dd45bd72a74c1e158f660508133db62d2cce
aliaitrahou <alirhou7@gmail.com>
2025-03-20 23:51:59 +0000

    refactor(api): use neon for semantic search

--- app/api/model/route.ts
+++ app/api/model/route.ts
@@ -1,41 +1,75 @@
 import { Together } from "together-ai";
-import { createClient } from "@supabase/supabase-js";
+import { Client } from "@neondatabase/serverless";
 
 const apiKey = process.env.TOGETHER_API_KEY;
 if (!apiKey) throw new Error("Missing Together env var");
-const sbApiKey = process.env.SUPABASE_API_KEY || "";
-const sbURL = process.env.SUPABASE_URL || "";
 
 export async function POST(req: Request) {
   const { message, chatHistory, model } = await req.json();
   const together = new Together({ apiKey });
+  const client = new Client({ connectionString: process.env.DATABASE_URL });
 
-  // Get the query embedding for the message using together ai embedding model
-  const response = await together.embeddings.create({
-    model: "togethercomputer/m2-bert-80M-8k-retrieval",
-    input: message,
-  });
+  const getEmbedding = async (text: string) => {
+    const response = await together.embeddings.create({
+      model: "togethercomputer/m2-bert-80M-8k-retrieval",
+      input: text,
+    });
 
-  // TODO: switch to neon victore daatabase
-  const supabase = createClient(sbURL, sbApiKey);
+    const q_embeddings = response["data"][0]["embedding"];
 
-  const { data, error } = await supabase.rpc("match_documents", {
-    query_embedding: response.data[0].embedding,
-    match_count: 2,
-  });
+    const q_embeddings_str = q_embeddings.toString().replace(/\.\.\./g, "");
+    return q_embeddings_str;
+  };
 
-  if (error) {
-    console.log(error);
-  }
+  const getContext = async (em: string) => {
+    // Query the database using your actual column names
+    const insertQuery = `
+      SELECT content
+      FROM pdf_embeddings
+      ORDER BY embedding <=> '[${em}]'
+      LIMIT 2; 
+      `;
+    console.log("Querying database...");
+    const { rows } = await client.query(insertQuery);
+    await client.end();
 
-  if (!data || data.length === 0) {
-    return new Response(
-      JSON.stringify({
-        message: "No relevant documents found. Please try refining your query.",
-      }),
-      { status: 404, headers: { "Content-Type": "application/json" } },
-    );
-  }
+    const context = rows.reduce((acc, cur) => {
+      return acc + cur.content;
+    }, "");
+
+    return context;
+  };
+
+  // const getContext = async (em: string) => {
+  // Query the database for the context
+  //     const insertQuery = `
+  //       SELECT content
+  // SELECT text, n_tokens, embeddings,
+  //       FROM (
+  //         (embeddings <=> '[${em}]') AS distances,
+  //         SUM(n_tokens) OVER (ORDER BY (embeddings <=> '[${em}]')) AS cum_n_tokens
+  //         FROM pdf_embeddings
+  //         ) subquery
+  //       WHERE cum_n_tokens <= $1
+  //       ORDER BY distances ASC;
+  //       `;
+
+  //   const queryParams = [1700];
+  //
+  //   console.log("Querying database...");
+  //   const { rows } = await client.query(insertQuery, queryParams);
+  //   await client.end();
+  //   const context = rows.reduce((acc, cur) => {
+  //     return acc + cur.text;
+  //   }, "");
+  //
+  //   return context;
+  // };
+
+  const embedding = await getEmbedding(message);
+  await client.connect();
+  const context = await getContext(embedding);
+  console.log("Context is : ", context);
 
   const prompt = process.env.MAGIC_PROMPT || "";
   const runner = together.chat.completions.stream({
@@ -43,9 +77,7 @@ export async function POST(req: Request) {
     messages: [
       {
         role: "system",
-        content:
-          prompt +
-          `CHAT_HISTORY:${chatHistory}\nCONTEXT: ${data.map((doc: { content: string }) => doc.content)}`,
+        content: prompt + `CHAT_HISTORY:${chatHistory}\nCONTEXT: `,
       },
       { role: "user", content: message },
     ],
